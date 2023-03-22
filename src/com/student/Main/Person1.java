7c0823b64938c3baa7306fc46ad677a5e7375ef9
truong-2802 <115643911+truong-2802@users.noreply.github.com>
2023-03-22 08:23:18 +0700

    commit

--- src/com/student/Main/Person1.java
+++ src/com/student/Main/Person1.java
@@ -7,28 +7,26 @@ public class Person1 {
     int id;
     String name;
     String email;
-    int age;
+
 
     public void display() {
-        System.out.println("Id " + this.id);
+
         System.out.println("Name " + this.name);
         System.out.println("Email " + this.email);
-        System.out.println("Age " + this.age);
+        System.out.println("Id " + this.id);
     }
 
     public void inputData() {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter id: ");
-        this.id = input.nextInt();
-
         System.out.print("Enter name: ");
         this.name = input.nextLine();
 
         System.out.print("Enter email: ");
         this.email = input.nextLine();
 
-        System.out.print("Enter age: ");
-        this.age = input.nextInt();
+        System.out.print("Enter id: ");
+        this.id = input.nextInt();
+
 
         System.out.println("Input completed");
     }
--- src/com/student/Main/Student.java
+++ src/com/student/Main/Student.java
@@ -3,7 +3,6 @@ package com.student.Main;
 public class Student {
     public static void main(String[] args) {
 
-        //Create objects
         Person1 person1 = new Person1();
         Person1 person2 = new Person1();
         Person1 person3 = new Person1();
@@ -18,14 +17,7 @@ public class Student {
         person3.display();
         person4.display();
 
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Please enter name: ");
-//        String name = input.nextLine();
-//
-//        System.out.print("Please enter mark: ");
-//        int mark = input.nextInt();
-//
-//        System.out.println("Hello " + name + ", your mark is = " + mark);
+
 
     }
 }
